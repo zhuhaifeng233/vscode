d594f502df26bd6ef29bf09d76ed0bec4c0972c2
zhuhaifeng233 <15142434171@163.com>
2020-03-31 15:08:48 +0800

    add js css

---
+++ html/htmljs.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    window.onload = function(){
+        var Lis = document.getElementsByTagName("li");
+        for (i = 0; i < Lis.length; i++) {
+            Lis[i].onmouseover = function(){
+                this.className = "lihover";
+            }
+
+            Lis[i].onmouseout = function(){
+                this.className = "";
+            }
+        }
+    }
+</script>
